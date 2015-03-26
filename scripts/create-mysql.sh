bc6995650c4e50686e90f91634fd0bfea06f5d77
Erik Belusic <erikbelusic@Eriks-Air.home>
2015-03-26 19:34:11 -0400

    change farmhouse back to homestead in db creation

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -2,5 +2,5 @@
 
 DB=$1;
 
-mysql -ufarmhouse -psecret -e "DROP DATABASE IF EXISTS \`$DB\`";
-mysql -ufarmhouse -psecret -e "CREATE DATABASE \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
+mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS \`$DB\`";
+mysql -uhomestead -psecret -e "CREATE DATABASE \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
