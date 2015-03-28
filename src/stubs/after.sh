4d4a92b2b6fa67abae93401a9e0daf241b41e81c
Erik Belusic <erikbelusic@Eriks-Air.home>
2015-03-27 20:14:31 -0400

    update after.sh to correctly install oh-my-zsh

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -4,9 +4,9 @@
 # add any commands you wish to this file and they will
 # be run after the Farmhouse machine is provisioned.
 
-if [[ -n $ZSH_VERSION ]]; then
-	
-	echo 'installing zsh and oh-my-zsh...'
+if [ ! -f /usr/local/extra_farmhouse_software_installed ]; then
+
+    echo 'installing some extra software...'
     #
     # install zsh
     #
@@ -19,5 +19,11 @@ if [[ -n $ZSH_VERSION ]]; then
     git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
     cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
     chsh -s /usr/bin/zsh vagrant
-
+    
+    #
+    # remember that the extra software is installed
+    #    
+    touch /usr/local/extra_farmhouse_software_installed
+else    
+    echo "extra software already installed... moving on..."
 fi
