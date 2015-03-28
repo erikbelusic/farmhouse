8f832f0be6cde1371e72afc97903ab69a2fb031a
Erik Belusic <erikbelusic@Eriks-Air.home>
2015-03-28 01:53:02 -0400

    oh-my-zsh to provision correctly. alter alias stub

--- scripts/farmhouse-provision.sh
+++ scripts/farmhouse-provision.sh
@@ -16,8 +16,6 @@ if [ ! -f /usr/local/extra_farmhouse_software_installed ]; then
     # (after.sh is run as the root user, but ssh is the vagrant user)
     #
     git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
-    cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
-    chsh -s /usr/bin/zsh vagrant
     
     #
     # remember that the extra software is installed
@@ -34,4 +32,10 @@ if [[ ! -d /home/vagrant/bin ]]; then
 	mkdir /home/vagrant/bin
 fi
 rsync -rvzh --delete /srv/config/homebin/ /home/vagrant/bin/
-echo " * rsync'd /srv/config/homebin                          to /home/vagrant/bin"
+echo " * rsync'd /srv/config/homebin TO /home/vagrant/bin"
+
+cp /srv/config/zshrc /home/vagrant/.zshrc
+echo " * Cposied: /srv/config/zshrc TO /home/vagrant/.zshrc"
+
+chsh -s /usr/bin/zsh vagrant
+echo "Changed shell TO /usr/bin/zsh for vagrant"
