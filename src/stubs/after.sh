b08f7c15e65ceb33941922ec978c6ebec046ad6a
Erik Belusic <erikbelusic@Eriks-Air.home>
2015-03-27 20:08:03 -0400

    install oh-my-zsh if not installed previously

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -3,3 +3,21 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Farmhouse machine is provisioned.
+
+if [[ -n $ZSH_VERSION ]]; then
+	
+	echo 'installing zsh and oh-my-zsh...'
+    #
+    # install zsh
+    #
+    apt-get install zsh -y
+    
+    #
+    # install oh my zhs
+    # (after.sh is run as the root user, but ssh is the vagrant user)
+    #
+    git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
+    cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
+    chsh -s /usr/bin/zsh vagrant
+
+fi
